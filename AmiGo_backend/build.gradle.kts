a70e07c47c10abc75d68f654606fe9c876c5924e
Adler John David - adlerjohn <adlerphotobusiness@gmail.com>
2023-09-17 01:49:30 +0200

    install dependencies

--- AmiGo_backend/build.gradle.kts
+++ AmiGo_backend/build.gradle.kts
@@ -19,9 +19,17 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.apache.httpcomponents:httpclient:4.5.13")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("mysql:mysql-connector-java")
+
+
 }
 
 tasks.withType<KotlinCompile> {
