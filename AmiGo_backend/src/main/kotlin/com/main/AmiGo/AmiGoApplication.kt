04fc359bd8693888b7b9f6d69ad922679d826a59
Adler John David - adlerjohn <adlerphotobusiness@gmail.com>
2023-09-17 01:36:06 +0200

    AmiGo init + backend init

---
+++ AmiGo_backend/src/main/kotlin/com/main/AmiGo/AmiGoApplication.kt
@@ -0,0 +1,11 @@
+package com.main.AmiGo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmiGoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AmiGoApplication>(*args)
+}
---
+++ AmiGo_backend/src/test/kotlin/com/main/AmiGo/AmiGoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.main.AmiGo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmiGoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
