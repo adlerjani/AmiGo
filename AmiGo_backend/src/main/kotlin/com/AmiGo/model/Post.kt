5441d743c13d9095556d1d37dcedbb3122afc9cb
Adler John David - adlerjohn <adlerphotobusiness@gmail.com>
2023-09-17 02:41:01 +0200

    create Post model

--- AmiGo_backend/src/main/kotlin/com/AmiGo/model/Post.kt
+++ AmiGo_backend/src/main/kotlin/com/AmiGo/model/Post.kt
@@ -1,2 +1,16 @@
 package com.AmiGo.model
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "post")
+class Post (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column
+        val id:Int=0,
+        @Column val content:String ="",
+        @Column val mediaContent:String ="",
+        @Column val created:String ="",
+        @OneToOne(mappedBy = "user", cascade = [CascadeType.PERSIST]) val userReference:User
+)
