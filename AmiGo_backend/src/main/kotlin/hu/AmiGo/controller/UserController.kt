49891889de382b2302f094b0d989100ae472441a
Adler John David - adlerjohn <adlerphotobusiness@gmail.com>
2023-09-18 02:04:07 +0200

    login done

--- AmiGo_backend/src/main/kotlin/hu/AmiGo/controller/UserController.kt
+++ AmiGo_backend/src/main/kotlin/hu/AmiGo/controller/UserController.kt
@@ -3,12 +3,16 @@ package hu.AmiGo.controller
 import hu.AmiGo.controller.dto.AuthUserRequestDto
 import hu.AmiGo.controller.dto.CreateUserRequestDto
 import hu.AmiGo.controller.dto.UserResponseDto
+import hu.AmiGo.model.User
 import hu.AmiGo.service.UserService
+import io.fusionauth.jwt.Verifier
 import io.fusionauth.jwt.domain.JWT
+import io.fusionauth.jwt.hmac.HMACVerifier
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+
 
 //@CrossOrigin
 @RestController
@@ -29,6 +33,27 @@ public class UserController (
         cookie.isHttpOnly = true
         response.addCookie(cookie)
         return "Success"
+    }
+
+    @GetMapping("/auth")
+    fun getAuthUser(@CookieValue("jwt") jwt:String?): Any {
+        if (jwt == null){
+            return "Authentication expired"
+        }
+        val verifier: Verifier = HMACVerifier.newVerifier("too many secrets")
+        val encode_jwt = JWT.getDecoder().decode(jwt, verifier)
+
+        return userService.getUserByUsernameForAuth(encode_jwt.issuer)
+//        return encode_jwt.issuer
+    }
+
+    @PostMapping("/logout")
+    fun postLogoutUser(response: HttpServletResponse): String{
+        val cookie = Cookie("jwt","")
+        cookie.maxAge=0
+
+        response.addCookie(cookie)
+        return "Logged Out";
     }
 
 }
